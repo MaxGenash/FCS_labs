945c722d00dc046c2d216d4c405599a039c421cb
MaxGenash <max_genash@mail.ru>
2015-09-21 23:24:13 +0300

    Wrote function for solving matrix of coincidence

--- app/js/main.js
+++ app/js/main.js
@@ -94,12 +94,72 @@
          * Повертає матрицю чисел
          */
         solveForm1: function(inpMatrix){
-            //ти заповниш цей массив під час обчислень, тому у присвоєння напишеш  = [[]];
-            var resultArr = [[ ]];
+            var i, j, k,                                     //ітератори
+                temp_i,temp_j,                               //тимчасові значення ітераторів
+                q,                                           //стільки рядків будемо перестрибувати
+                count,                                       //кількість співпадінь
+                rows = inpMatrix.length,                     //максимальна кількість рядків
+                cols = inpMatrix[0].length,                  //максимальна кількість стовпців
+                resultArr = new Array( inpMatrix.length),
+                numOfUniq = app.numOfuniqueInArr(inpMatrix); //кількість унікальних елементів матриці
 
-            //тут буде алгоритм
+            //Алгоритм обрахунку матриці співпадінь
+            for(i = 0;i < rows; i++){
+                temp_i=i;
+                q = 1;
+                count = 0;
+                resultArr[i] = new Array(cols);
+                while (i+q<rows){
+                    for(j=0;j<cols;j++){
+                        temp_j=j;
+                        for(k=0;k<cols;k++){
+                            if(( inpMatrix[temp_i][temp_j] === inpMatrix[temp_i+q][k] )&&(inpMatrix[temp_i][temp_j]!=''))
+                               break;
+                            else if((k+1==cols)&&(inpMatrix[temp_i][temp_j]!=''))
+                                count++;
+                        }
+                        for(k=0;k<cols;k++){
+                            if(( inpMatrix[temp_i+q][temp_j] === inpMatrix[temp_i][k] )&&(inpMatrix[temp_i+q][temp_j]!=''))
+                                break;
+                            else if((k+1==cols)&&(inpMatrix[temp_i+q][temp_j]!=''))
+                                count++;
+                        }
+                    }
+                    resultArr[temp_i][temp_i+q] = numOfuniqueInArr - count;
+                    count=0;
+                    q++;
+                }
+            }
 
-            return inpMatrix; //resultArr;
+            //дозаповнення матриці потрібними значеннями
+            for(i=0; i < rows; i++ ){
+                for(j=0; j < cols; j++ ){
+                    if( i === j) resultArr[i][j] = "-";
+                    if( resultArr[i][j] === undefined ) resultArr[i][j] = " ";
+                }
+            }
+            return resultArr;
+        },
+        // кількість унікальних елементів у масиві будь-якої розмірності
+        numOfuniqueInArr: function unique(arr) {
+            var obj = {};   //допоміжний об'єкт, куди записуються елементи масиву як унікальні ключі
+
+            (function writeInObjUniqueVal(arr){
+                if(arr.length === 0) return 0;
+
+                arr.forEach(function(item, i, arr){
+                    if(item instanceof Array){
+                        writeInObjUniqueVal(item);
+                        return 0;   //коли пройдемо по всіх елементах масива, щоб сам цей масив не записало як ключ
+                    }
+
+                    var str = item.toString();  //на випадок якщо це функція, дата, тощо
+                    if (str !== '')
+                        obj[str] = true; // запомнить строку в виде свойства объекта
+                });
+            }(arr));
+
+            return Object.keys(obj).length;
         },
 
         //метод для валідації форми поки не використовується
